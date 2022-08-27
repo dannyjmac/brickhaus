671bea034686b0c1e1caf2d07649f931732f6c00
Daniel McGrane <daniel.mcgrane@zebedee.io>
2022-08-27 14:23:10 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "mapbox-gl/dist/mapbox-gl.css";
+// @ts-ignore
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import { useRef, useEffect } from "react";
+import data from "./TN.json";
+
+const home = [0.557299, 50.857839];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const mapContainer = useRef(null);
+  const map = useRef<any>(null);
+
+  const randomColor = () => {
+    return (
+      "hsl(" +
+      360 * Math.random() +
+      "," +
+      (25 + 70 * Math.random()) +
+      "%," +
+      (50 + 10 * Math.random()) +
+      "%)"
+    );
+  };
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      zoom: 12,
+      center: home,
+    });
+
+    map.current.on("load", function () {
+      data.features.map((item, index) => {
+        console.log({ item });
+        map.current.addSource(`tn-${index}`, {
+          type: "geojson",
+          data: item,
+        });
+
+        map.current.addLayer({
+          id: `tn-label-${index}`,
+          type: "symbol",
+          source: `tn-${index}`,
+          layout: {
+            "text-field": ["get", "name"],
+            "text-variable-anchor": ["top", "bottom", "left", "right"],
+            "text-radial-offset": 0.5,
+            "text-justify": "auto",
+            "icon-image": ["get", "icon"],
+          },
+        });
+
+        map.current.addLayer({
+          id: `tn-layer-${index}`,
+          type: "fill",
+          source: `tn-${index}`,
+          paint: {
+            "fill-color": randomColor(),
+            "fill-opacity": 0.4,
+            "fill-antialias": true,
+            "fill-outline-color": "black",
+          },
+        });
+      });
+    });
+  });
+  return <div style={{ height: "100vh", width: "100vw" }} ref={mapContainer} />;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// @ts-ignore
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
