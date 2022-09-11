58873a6ab7fdc1fb75173b605a4dac62d59a09fb
Daniel McGrane <daniel.mcgrane@zebedee.io>
2022-09-11 10:13:12 +0100

    mvp map in plac

--- src/App.tsx
+++ src/App.tsx
@@ -1,169 +1,65 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 // @ts-ignore
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-import { useRef, useEffect, useState } from "react";
-import { getRandomColor } from "./utils/getRandomColor";
-import * as areas from "./areas";
-import * as districts from "./districts";
-import { filterDistricts } from "./utils/filterGeoData";
+import { useRef, useEffect } from "react";
+import { areaOpacity, postcodeLabels, fills } from "./utils/map";
 
 const center = [-0.118092, 51.509865];
 
 function App() {
   const mapContainer = useRef(null);
   const map = useRef<any>(null);
-  const [filteredDistricts, setFilteredDistricts] = useState<string[]>([]);
-  const [mapLoaded, setMapLoaded] = useState(false);
-
-  const [activeDistrict, setActiveDistrict] = useState<any>("");
-
-  const applyAreas = () => {
-    Object.keys(areas).forEach((area: string) => {
-      // @ts-ignore
-      areas[area].features.forEach((item) => {
-        if (map.current.getSource(area)) return;
-        map.current.addSource(area, {
-          type: "geojson",
-          // @ts-ignore
-          data: item,
-        });
-
-        map.current.addLayer({
-          id: `${area}-layer`,
-          type: "fill",
-          source: area,
-          tolerance: 0,
-          minzoom: 2,
-          // maxzoom: 10,
-          paint: {
-            "fill-color": getRandomColor(),
-            "fill-opacity": 0.5,
-            "fill-antialias": true,
-            "fill-outline-color": "black",
-          },
-        });
-
-        map.current.on("mouseenter", `${area}-layer`, (e: any) => {
-          map.current.setPaintProperty(`${area}-layer`, "fill-opacity", 0.6);
-        });
-
-        map.current.on("mouseleave", `${area}-layer`, () => {
-          map.current.setPaintProperty(`${area}-layer`, "fill-opacity", 0.3);
-        });
-
-        map.current.on("click", `${area}-layer`, async () => {
-          Object.keys(areas).forEach((area2: string) => {
-            if (area2 !== area) {
-              map.current.setLayoutProperty(
-                `${area2}-layer`,
-                "visibility",
-                "visible"
-              );
-            }
-          });
-
-          await map.current.setLayoutProperty(
-            `${area}-layer`,
-            "visibility",
-            "none"
-          );
-          applyDistricts(area);
-        });
-      });
-    });
-  };
-
-  const applyDistricts = (area: string) => {
-    if (map.current.getSource(`tn-1`)) {
-      console.log("found one");
-    }
-
-    Object.keys(areas).forEach((currentArea) => {
-      // @ts-ignore
-      if (districts[currentArea]?.features) {
-        // @ts-ignore
-        districts[currentArea].features.forEach((item: any, index: any) => {
-          if (map.current.getSource(`${currentArea}-${index}`)) {
-            map.current.removeLayer(`${currentArea}-layer-${index}`);
-            map.current.removeSource(`${currentArea}-${index}`);
-          }
-        });
-      }
-    });
-
-    // @ts-ignore
-    districts[area].features.forEach((item: any, index: any) => {
-      if (map.current.getSource(`${area}-${index}`)) return;
-      map.current.addSource(`${area}-${index}`, {
-        type: "geojson",
-        data: item,
-      });
-      map.current.addLayer({
-        id: `${area}-layer-${index}`,
-        type: "fill",
-        source: `${area}-${index}`,
-        tolerance: 0,
-        paint: {
-          "fill-color": getRandomColor(),
-          "fill-opacity": 0.4,
-          "fill-antialias": true,
-          "fill-outline-color": "black",
-        },
-      });
-    });
-
-    setActiveDistrict(area);
-  };
 
   useEffect(() => {
     if (map.current) return;
 
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
+      style: "mapbox://styles/dannyjmac/cl7wa5w51008e14kbusgohndx",
       zoom: [8],
       maxZoom: 14,
       center,
     });
 
-    map.current.on("moveend", (e: any) => {
-      if (e.target.transform.tileZoom < 9) return;
-      setFilteredDistricts(filterDistricts(e.target.transform._center));
+    let hovered: any = null;
+
+    map.current.on("mousemove", "postcodes", (e: any) => {
+      if (hovered !== null) {
+        map.current.setFeatureState(
+          {
+            source: "composite",
+            sourceLayer: "postcodes",
+            id: hovered,
+          },
+          {
+            hover: false,
+          }
+        );
+      }
+      if (e.features) {
+        map.current.setFeatureState(
+          {
+            source: "composite",
+            sourceLayer: "postcodes",
+            id: e.features[0].id,
+          },
+          {
+            hover: true,
+          }
+        );
+        hovered = e.features[0].id;
+      }
     });
 
     map.current.on("load", () => {
-      setMapLoaded(true);
+      map.current.setPaintProperty("postcodes", "fill-color", fills);
+      map.current.setPaintProperty("postcodes", "fill-outline-color", "black");
+      map.current.setPaintProperty("postcodes", "fill-opacity", areaOpacity);
+      map.current.addLayer(postcodeLabels);
     });
-
-    // Use for cleaning up districts
-    // map.current.on("zoom", (e: any) =>
-    //   handleOnZoom(e.target.transform.tileZoom)
-    // );
   });
-
-  useEffect(() => {
-    if (!mapLoaded) return;
-    applyAreas();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [mapLoaded, filteredDistricts]);
 
   return <div style={{ height: "100vh", width: "100vw" }} ref={mapContainer} />;
 }
 
 export default App;
-
-// map.current.addLayer({
-//   id: `${region}-label-${index}`,
-//   type: "symbol",
-//   source: `${region}-${index}`,
-//   // filter: ["<=", ["distance-from-center"], 0.5],
-//   minzoom: 7,
-//   // tolerance: 0.1,
-//   layout: {
-//     "text-field": ["get", "name"],
-//     "text-variable-anchor": ["top", "bottom", "left", "right"],
-//     "text-radial-offset": 0.5,
-//     "text-justify": "auto",
-//     "icon-image": ["get", "icon"],
-//   },
-// });
---
+++ src/utils/map.ts
@@ -0,0 +1,90 @@
+export const text = {
+  "text-field": [
+    "format",
+    ["upcase", ["get", "district"]],
+    { "font-scale": 0.8 },
+  ],
+  "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+};
+
+export const areaOpacity = [
+  "case",
+  ["boolean", ["feature-state", "hover"], false],
+  0.6,
+  0.2,
+];
+
+export const postcodeLabels = {
+  id: "postcode-labels",
+  type: "symbol",
+  source: "composite",
+  "source-layer": "postcodes",
+  layout: text,
+};
+
+export const fills = [
+  "interpolate",
+  ["linear"],
+  ["get", "group"],
+  0,
+  "hotpink",
+  10,
+  "yellow",
+  20,
+  "orange",
+  30,
+  "red",
+  40,
+  "yellow",
+  50,
+  "hotpink",
+  60,
+  "yellow",
+  70,
+  "orange",
+  80,
+  "red",
+  90,
+  "yellow",
+  100,
+  "hotpink",
+  110,
+  "red",
+  120,
+  "orange",
+];
+
+// map.current.setPaintProperty(
+//   "postcodes",
+//   "fill-color",
+
+//   [
+//     "match",
+//     ["get", "group"],
+//     0,
+//     "#fbb03b",
+//     1,
+//     "#223b53",
+//     2,
+//     "#e55e5e",
+//     3,
+//     "#3bb2d0",
+//     /* other */ "#ccc",
+//   ]
+// );
+
+// Interpolate Linear
+//   "interpolate",
+//   ["linear"],
+//   ["get", "group"],
+//   0,
+//   "blue",
+//   20,
+//   "orange",
+//   50,
+//   "red",
+//   90,
+//   "green",
+//   120,
+//   "yellow",
+// ]);
